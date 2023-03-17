dec595fc35de5ce60e903ae08cc1d7d20cdcea8d
Christian Medeiros <christianmedeiros022@gmail.com>
2023-03-16 23:22:36 -0300

    20230316.0
    commit inicial
    + estrutura inicial do projeto

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Logo from "../assets/logo.svg";
+import background from "../assets/divback.svg";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.headerIndex}>
+      <img src={Logo}/>
+      <ul>
+        <li>Inicio</li>
+        <li>Tecnocar</li>
+        <li>Serviços</li>
+        <li>Agendamento</li>
+        <li>Contato</li>
+        <li>medeiros</li>
+      </ul>
+      <img src={background} />
+    </header>
+  );
+};
+
+export default Header;
