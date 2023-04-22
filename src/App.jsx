0e4313f94a91918cb99d2b6e0c32b357645f6357
Christian Medeiros <christianmedeiros022@gmail.com>
2023-04-22 00:04:02 -0300

    solução

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import Analytics from '@vercel/analytics';
-Analytics.initialize({ projectID: 'prj_iRf1RPrgwcbGxy2ArqkpIDUiiQPP' });
-
 import "./App.css";
 import Header from "./components/headerTecnocar/Header";
 import SectionTecnocar from "./components/sectionTecnocar/SectionTecnocar";
@@ -11,7 +8,15 @@ import SectionScheduling from "./components/sectionScheduling/SectionScheduling"
 import SectionMap from "./components/sectionMap/SectionMap";
 import SectionContact from "./components/sectionContact/SectionContact";
 import SectionCredit from "./components/sectionCredit/SectionCredit";
+import analytics from "@vercel/analytics";
+
 function App() {
+  const projectId = "prj_iRf1RPrgwcbGxy2ArqkpIDUiiQPP";
+  if (process.env.NODE_ENV === 
+  
+  'production') {
+  analytics(projectId);
+  }
   return (
     <div className="App">
       <Header />
