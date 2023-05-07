17135f2b67574d21b0a54f38afddeb3453294be6
Christian medeiros <christianmedeiros022@gmail.com>
2023-05-07 12:50:00 -0300

    adicionando mapa
    adicionado mapa
    alteração no layout

--- src/components/sectionMap/SectionMap.jsx
+++ src/components/sectionMap/SectionMap.jsx
@@ -2,21 +2,26 @@ import styles from "../sectionMap/SectionMap.module.css";
 import Emoji from "../../assets/iconMaps.svg";
 
 function Mapa() {
-    return (
-        <div className={styles.containerMap}>
-            <div className={styles.GoWorkshop}>
-                <a
-                    href="https://www.google.com/maps/search/?api=1&query=-30.860908945450138,-51.82581726097299"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <button className={styles.buttonClick}>
-                        <img src={Emoji} alt="logo do google maps"/> IR ATÉ A OFICINA
-                    </button>
-                </a>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.containerMap}>
+      <div className={styles.GoWorkshop}>
+        <a
+          href="https://www.google.com/maps/search/?api=1&query=-30.860908945450138,-51.82581726097299"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className={styles.buttonClick}>
+            <img src={Emoji} alt="logo do google maps" /> IR ATÉ A OFICINA
+          </button>
+        </a>
+      </div>
+      <iframe
+        className={styles.maps}
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3424.9170151138273!2d-51.8258619!3d-30.8609983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x951a77115f6ec6cd%3A0xe3f85d9727c45d20!2sR.%20Arl%C3%ADndo%20Cardoso%2C%20195%20-%20Maria%20da%20Gra%C3%A7a%2C%20Camaqu%C3%A3%20-%20RS%2C%2096180-000!5e0!3m2!1spt-BR!2sbr!4v1683337305565!5m2!1spt-BR!2sbr"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
 }
 
 export default Mapa;
--- src/components/sectionScheduling/SectionScheduling.jsx
+++ src/components/sectionScheduling/SectionScheduling.jsx
@@ -2,24 +2,25 @@ import styles from "./SectionScheduling.module.css";
 import Emoji from "../../assets/i2.svg";
 
 const Location = () => {
-    return (
-        <div id="location" className={styles.contentSchenduling}>
-            <div className={styles.text}>
-                <p>Faça seu agendamento agora mesmo clicando no botão abaixo:</p>
+  return (
+    <div id="location" className={styles.contentSchenduling}>
+      <div className={styles.text}>
+        <p>Faça seu agendamento agora mesmo clicando no botão abaixo:</p>
 
-                <button className={styles.buttonClick}>
-                    <a
-                        href="https://api.whatsapp.com/send?phone=5551995592253&text=Ol%C3%A1.%20Gostaria%20de%20fazer%20um%20agendamento%20para%20meu%20ve%C3%ADculo.%20%C3%89%20poss%C3%ADvel?"
-                        target="_blank"
-                    >
-                        <img src={Emoji} alt="emoji de dedo para clicar"/> Faça seu orçamento
-                    </a>
-                </button>
-            </div>
+        <button className={styles.buttonClick}>
+          <a
+            href="https://api.whatsapp.com/send?phone=5551995592253&text=Ol%C3%A1.%20Gostaria%20de%20fazer%20um%20agendamento%20para%20meu%20ve%C3%ADculo.%20%C3%89%20poss%C3%ADvel?"
+            target="_blank"
+          >
+            <img src={Emoji} alt="emoji de dedo para clicar" /> Faça seu
+            orçamento
+          </a>
+        </button>
+      </div>
 
-            <div className={styles.workshopPhoto}/>
-        </div>
-    );
+      <div className={styles.workshopPhoto} />
+    </div>
+  );
 };
 
 export default Location;
